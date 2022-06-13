a15fbea6f737fa528805cbb5ca7e6ed0399103b6
vdsuN24yn6K3m <lightcoder22@gmail.com>
2022-06-13 13:18:30 +0300

    todo-app refactor

--- todo-app/src/components/TaskSection/taskControls/index.tsx
+++ todo-app/src/components/TaskSection/taskControls/index.tsx
@@ -1,7 +1,7 @@
 import { useState, useRef } from "react";
 import { useDispatch } from "react-redux";
 import burger from "../../../burger.svg";
-import PopupOverlay from "../../Modals/PopupOverlay";
+import PopupOverlay from "../../Modals/popupOverlay";
 import styles from "./taskControls.module.css";
 
 type PropsType = {
